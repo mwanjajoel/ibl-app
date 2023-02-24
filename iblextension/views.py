beb6a62df19e55b0a5e0446b6923f77a63d86dc3
Mwanja Joel <joelsilverworks@gmail.com>
2023-02-24 19:13:48 +0300

    fixed minor issues

--- iblextension/views.py
+++ iblextension/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
 from oauth2_provider.decorators import protected_resource
 from .models import Greeting
@@ -7,22 +7,27 @@ from rest_framework.decorators import api_view
 @csrf_exempt
 @protected_resource()
 @api_view(['POST'])
-def save_greeting(request):
-    if request.method == 'POST':
-        print("The request", request.data)
-        text = request.data.get('text')
+def save_greeting(greeting):
+    if greeting.method == 'POST':
+        print("The request", greeting.data)
+        text = greeting.data.get('text')
         if text:
             print(text)
             try:
-                greeting = Greeting(text=text)
-                greeting.save()
+                the_greeting = Greeting(text=text)
+                the_greeting.save()
             except Exception as e:
                 print(e)
                 return JsonResponse({'message': 'Unable to save greeting.'})
             if text.lower() == 'hello':
-                response = request.get(os.getenv("BASE_API") + '?text=goodbye', headers={
+                try:
+                    response = HttpRequest.GET(os.getenv("BASE_API") + '?text=goodbye', headers={
                     'Authorization': 'Bearer {}'.format(request.access_token.token),
-                })
+                    })
+                    print(response)
+                except Exception as e:
+                    print(e)
+                    return JsonResponse({'message': 'There was a server error, its not you, its us'})
                 return JsonResponse({'message': 'Greeting saved and original greeting called again with "goodbye".'})
             else:
                 return JsonResponse({'message': 'Greeting saved.'})
