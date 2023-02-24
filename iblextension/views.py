eecd3f3eade534bac86fed3e6abf7866c47a685a
Mwanja Joel <joelsilverworks@gmail.com>
2023-02-24 19:24:53 +0300

    fixed original responses issues

--- iblextension/views.py
+++ iblextension/views.py
@@ -3,6 +3,8 @@ from django.views.decorators.csrf import csrf_exempt
 from oauth2_provider.decorators import protected_resource
 from .models import Greeting
 from rest_framework.decorators import api_view
+import requests
+import os
 
 @csrf_exempt
 @protected_resource()
@@ -21,12 +23,16 @@ def save_greeting(greeting):
                 return JsonResponse({'message': 'Unable to save greeting.'})
             if text.lower() == 'hello':
                 try:
-                    response = HttpRequest.GET(os.getenv("BASE_API") + '?text=goodbye', headers={
-                    'Authorization': 'Bearer {}'.format(request.access_token.token),
-                    })
-                    print(response)
+                    url = os.getenv("BASE_API") + '?text=goodbye'
+                    access_token = greeting.data.get('access_token')
+                    print("The access token", access_token)
+                    headers = {
+                        'Authorization': 'Bearer {}'.format(access_token),
+                    }
+                    response = requests.get(url, headers=headers)
+                    print("The response", response.json())
                 except Exception as e:
-                    print(e)
+                    print("The error: ==>", e)
                     return JsonResponse({'message': 'There was a server error, its not you, its us'})
                 return JsonResponse({'message': 'Greeting saved and original greeting called again with "goodbye".'})
             else:
